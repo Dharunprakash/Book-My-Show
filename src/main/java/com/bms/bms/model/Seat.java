b67ac9410d4b5bebd57a2a09ce42e9745e96392d
Dharunprakash <dharunprakash.m1504@gmail.com>
2024-10-05 10:55:18 +0530

    refactor: model, fixed missed relations

--- src/main/java/com/bms/bms/model/Seat.java
+++ src/main/java/com/bms/bms/model/Seat.java
@@ -1,67 +1,17 @@
 package com.bms.bms.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Seat {
     private Long id;
     private Long screenId;
-    private int seatNumber;
-
-    private Seat(SeatBuilder builder) {
-        this.id = builder.id;
-        this.screenId = builder.screenId;
-        this.seatNumber = builder.seatNumber;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public Long getScreenId() {
-        return screenId;
-    }
-
-    public int getSeatNumber() {
-        return seatNumber;
-    }
-
-
-    public static class SeatBuilder {
-        private Long id;
-        private Long screenId;
-        private int seatNumber;
-
-
-        public SeatBuilder setId(Long id) {
-            this.id = id;
-            return this;
-        }
-
-
-        public SeatBuilder setScreenId(Long screenId) {
-            this.screenId = screenId;
-            return this;
-        }
-
-
-        public SeatBuilder setSeatNumber(int seatNumber) {
-            this.seatNumber = seatNumber;
-            return this;
-        }
-
-
-        public Seat build() {
-            return new Seat(this);
-        }
-    }
-
-
-    @Override
-    public String toString() {
-        return "Seat{" +
-                "id=" + id +
-                ", screenId=" + screenId +
-                ", seatNumber=" + seatNumber +
-                '}';
-    }
+    private Integer seatNumber;
+    private List<BookingSeats> bookedSeats;
 }
