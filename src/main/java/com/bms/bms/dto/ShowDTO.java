7a012b707eb1bc8085ee5c01658b5642bcfbfdb4
dharun <dharunprakash.m1504@gmail.com>
2024-10-05 20:53:09 +0530

    feat: added show dto

--- src/main/java/com/bms/bms/dto/ShowDTO.java
+++ src/main/java/com/bms/bms/dto/ShowDTO.java
@@ -9,6 +9,6 @@ import java.util.List;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class ScreeningDTO extends TheatreDTO {
+public class ShowDTO extends MovieDTO {
     private List<ScreenShowtimeDTO> showtimeDetails;
 }
